dfbeb89f9bbaf6854393b456c1320a92ef875e7c
rafat69ahmed <rafat69ahmed@gmail.com>
2021-11-11 18:04:21 +0600

    full done

--- src/component/page/product.jsx
+++ src/component/page/product.jsx
@@ -7,18 +7,49 @@ import { SearchOutlined } from '@ant-design/icons';
 const Product = () => {
     const columns = [
         {
-          title: 'Code',
-          dataIndex: 'code',
-          key: 'code',
-          width: '20%',
-        //   ...this.getColumnSearchProps('age'),
+            title: 'Code',
+            dataIndex: 'code',
+            key: 'code',
+            width: '10%',
+            //   ...this.getColumnSearchProps('age'),
         },
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-          width: '30%',
-        //   ...this.getColumnSearchProps('name'),
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            width: '15%',
+            //   ...this.getColumnSearchProps('name'),
+        },
+        {
+            title: 'Availability',
+            dataIndex: 'availability',
+            key: 'availability',
+            width: '10%',
+            render: availability => (
+                <>
+                    {availability.toString()}
+                </>
+            ),
+            //   ...this.getColumnSearchProps('name'),
+        },
+        {
+            title: 'Durability',
+            dataIndex: 'durability',
+            key: 'durability',
+            width: '15%',
+            //   ...this.getColumnSearchProps('name'),
+        },
+        {
+            title: 'Mileage',
+            dataIndex: 'mileage',
+            key: 'mileage',
+            width: '15%',
+            render: mileage => (
+                <>
+                    {mileage ? mileage : 'null'}
+                </>
+            )
+            //   ...this.getColumnSearchProps('name'),
         }
         // {
         //   title: 'Address',
@@ -28,7 +59,7 @@ const Product = () => {
         //   sorter: (a, b) => a.address.length - b.address.length,
         //   sortDirections: ['descend', 'ascend'],
         // },
-      ];
+    ];
     return (
         // <div>
         //     {
